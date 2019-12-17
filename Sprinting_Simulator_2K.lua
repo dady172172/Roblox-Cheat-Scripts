e4fc1062c51d7ae887499cae9a1718cfaab0f827
dady172172 <dady172172@gmail.com>
2019-12-17 07:47:59 -0500

    first upload

---
+++ Sprinting_Simulator_2K.lua
@@ -0,0 +1,12 @@
+--[[
+game https://www.roblox.com/games/4003645442/Sprinting-Simulator-2K
+more https://github.com/dady172172/Roblox-Cheats
+]]--
+
+_G.h = true
+while _G.h == true do 
+	wait()
+	game.ReplicatedStorage.SwitchLeaderstats:FireServer("Speed", 99999999999999999)
+	game.ReplicatedStorage.SwitchStatsValueAdd:FireServer("Cash", 99999999999999999)
+	game.ReplicatedStorage.SwitchLeaderstats:FireServer("Rebirths", 99999999999999999)
+end
