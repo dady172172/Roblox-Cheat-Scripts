abd860d3879a32a9b99a5a7663fa917e5993df6c
Kethunsar <dady172172@gmail.com>
2020-05-15 11:58:44 -0400

    Create Coin Simulator.lua

---
+++ Coin Simulator.lua
@@ -0,0 +1,16 @@
+--[[
+Game https://www.roblox.com/games/4767544262/Coin-Simulator
+contact https://github.com/dady172172/Roblox-Cheats
+]]--
+--collects coins and can ruin the game for other people
+_G.shit = true
+while _G.shit == true do
+	for _,v in pairs(game:GetService("Workspace"):GetChildren()) do
+		if string.match(v.Name, "Coin") then
+			if v:FindFirstChild("TouchInterest") then
+				wait()
+				firetouchinterest(game.Players.LocalPlayer.Character.LeftFoot, v, 0)
+			end
+		end
+	end
+end
