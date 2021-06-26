9b6599041abfb9c79273de7f84961332a82a465d
Kethunsar <dady172172@gmail.com>
2021-06-25 22:52:32 -0400

    update/Hotfix

--- fishing_simulator_gui.lua
+++ fishing_simulator_gui.lua
@@ -1,5 +1,5 @@
 --[[
-Game :
+Game : https://www.roblox.com/games/3329474278/FISHING-SIMULATOR?
 Codded by : Keathunsar : https://github.com/dady172172/Roblox-Cheats
 Gui made by : https://v3rmillion.net/member.php?action=profile&uid=507120
 Go vouch release thread : https://v3rmillion.net/showthread.php?tid=1040650
@@ -31,7 +31,7 @@ sectionFarm:Toggle("Sell",function(bool)
 end)
 function sellFunc()
     spawn(function()
-        while sellBool do
+        while kVars.sellBool do
             game.ReplicatedStorage.Connections.FishingSellAll:InvokeServer()
         end
     end)
@@ -44,7 +44,7 @@ sectionFarm:Toggle("Refill Bait",function(bool)
 end)
 function refillBaitFunc()
     spawn(function()
-        while refillBaitBool do
+        while kVars.refillBaitBool do
             wait()
             local baitAmount = tonumber(string.match(game:GetService("Players").LocalPlayer.PlayerGui.ScreenGui.SideBait.Counter.text, "%d+"))
             if (baitAmount <= 2) then
@@ -94,66 +94,3 @@ kVars.closeing = game:GetService("CoreGui").ChildRemoved:Connect(function(child)
 
 	end
 end)
-
-
---[[
-b:Label("Pretty Useless NGL",{
-    TextSize = 25; -- Self Explaining
-    TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
-    BgColor = Color3.fromRGB(69,69,69); -- Self Explaining
-    
-}) 
-
-b:Button("Button",function()
-    print("Elym Winning")
-end)
-
-b:Toggle("Toggle",function(bool)
-    shared.toggle = bool
-    print(shared.toggle)
-end)
-
-b:Slider("Slider",{
-    min = 10; -- min value of the slider
-    max = 50; -- max value of the slider
-    precise = true; -- max 2 decimals
-},function(value)
-    print(value)
-end)
-
-b:Dropdown("Dropdown",{"A","B","C"},true,function(mob) --true/false, replaces the current title "Dropdown" with the option that t
-    print(mob)
-end)
-
-b:Bind("Bind",Enum.KeyCode.C,function() --Default bind
-    print("Yes")
-end)
-
-b:ColorPicker("ColorPicker",Color3.fromRGB(255,0,0),function(color) --Default color
-    print(color)
-end)
-
-b:Box("Box","number",function(value) -- "number" or "string"
-    print(value)
-end)
-
-b:DestroyGui()
-
-
-How to refresh a dropdown:
-1)Create the dropdown and save it in a variable
-local yourvariable = b:Dropdown("Hi",yourtable,function(a)
-    print(a)
-end)
-2)Refresh it using the function
-yourvariable:Refresh(yourtable)
-How to refresh a label:
-1)Create your label and save it in a variable
-local yourvariable = b:Label("Pretty Useless NGL",{
-    TextSize = 25; -- Self Explaining
-    TextColor = Color3.fromRGB(255,255,255);
-    BgColor = Color3.fromRGB(69,69,69);
-})
-2)Refresh it using the function
-yourvariable:Refresh("Hello") It will only change the text ofc
-]]
