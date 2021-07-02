f3498dfea84d590c1b8e8b47a4141a2b7d86d1cc
Kethunsar <dady172172@gmail.com>
2021-07-02 09:47:28 -0400

    Update

--- Jetpack Simulator GUI.lua
+++ Jetpack Simulator GUI.lua
@@ -4,10 +4,9 @@ Codded by : Keathunsar : https://github.com/dady172172/Roblox-Cheats
 Gui made by : https://v3rmillion.net/member.php?action=profile&uid=507120
 Go vouch release thread : https://v3rmillion.net/showthread.php?tid=1040650
 ]]--
-local VirtualUser=game:service'VirtualUser'
 game:service'Players'.LocalPlayer.Idled:connect(function()
-    VirtualUser:CaptureController()
-    VirtualUser:ClickButton2(Vector2.new())
+    game:service'VirtualUser':CaptureController()
+    game:service'VirtualUser':ClickButton2(Vector2.new())
 end)
 
 local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3'))()
