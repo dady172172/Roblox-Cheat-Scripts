173625928393876afb92d98b5ea06eb2d7abbf04
Kethunsar <dady172172@gmail.com>
2021-07-18 13:49:02 -0400

    Upload

---
+++ Power Wash Simulator.lua
@@ -0,0 +1,7 @@
+--[[
+Game : https://www.roblox.com/games/7009799230
+Codded by : Keathunsar : https://github.com/dady172172/Roblox-Cheats
+]]--
+
+--- refill water / Inf Water... if you refill your tank manually, you will have to run this again.
+game:GetService("ReplicatedStorage").Remotes.RefillRemote:FireServer(true)
