8a4d58e5193b4c035ce5304e3609ce02ab367f5d
Kethunsar <dady172172@gmail.com>
2022-02-14 20:05:44 -0500

    Updated GUI Added teleport by name Added walkspeed, jumppower, infjump
    Signed-off-by: Kethunsar <dady172172@gmail.com>

--- Metal Detecting Simulator GUI.lua
+++ Metal Detecting Simulator GUI.lua
@@ -1,27 +1,83 @@
 --[[
 Game : https://www.roblox.com/games/1509515037
-Codded by : Keathunsar : https://github.com/dady172172/Roblox-Cheats
-Gui made by : https://v3rmillion.net/member.php?action=profile&uid=507120
-Go vouch release thread : https://v3rmillion.net/showthread.php?tid=1040650
+Coded by : Keathunsar : https://github.com/dady172172/Roblox-Cheats
+Gui made by : https://v3rmillion.net/member.php?action=profile&uid=244024
 ]]--
-local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3'))()
+
+---- Variables ----
 kVars = {}
-kVars.windowName = "Metal Detecting Simulator GUI" -- Window Name here
-local w = library:CreateWindow(kVars.windowName) -- Creates the window
-local f = w:CreateFolder("Farm") -- Creates the folder(U will put here your buttons,etc)
+kVars.WindowName = "Metal Detecting Simulator GUI"
+kVars.txtName = kVars.WindowName .. ".txt"
+kVars.lp = game:GetService('Players').LocalPlayer
+kVars.rs = game:GetService('ReplicatedStorage')
+kVars.vu = game:GetService('VirtualUser')
+kVars.HttpService = game:GetService("HttpService")
+kVars.themes = {}
+kVars.tmp = {}
+---- Load Settings ----
+if readfile and isfile and isfile(kVars.txtName) then
+    kVars.tmp = kVars.HttpService:JSONDecode(readfile(kVars.txtName))
+    for i,v in pairs(kVars.tmp) do
+        kVars.themes[i] = Color3.fromRGB(v[1], v[2], v[3])
+    end
+else
+    print("Loading defult theme. Your injector does not support readfile or isfile.")
+    kVars.themes = {
+        Background = Color3.fromRGB(24, 24, 24),
+        Glow = Color3.fromRGB(0, 0, 0),
+        Accent = Color3.fromRGB(10, 10, 10),
+        LightContrast = Color3.fromRGB(20, 20, 20),
+        DarkContrast = Color3.fromRGB(14, 14, 14),  
+        TextColor = Color3.fromRGB(255, 255, 255)
+   }
+end
 
---- anti afk ---
-local VirtualUser=game:service'VirtualUser'
-game:service'Players'.LocalPlayer.Idled:connect(function()
-    VirtualUser:CaptureController()
-    VirtualUser:ClickButton2(Vector2.new())
+---- Save Settings ----
+function saveSettings()
+    if writefile then
+        kVars.themes = {}
+        for i,v in pairs(game:GetService('CoreGui')[kVars.WindowName]:GetChildren()) do
+            if v.Name == "ColorPicker" then
+                kVars.themes[v.Title.Text] = {v.Container.Inputs.R.Textbox.text, v.Container.Inputs.G.Textbox.text, v.Container.Inputs.B.Textbox.text}
+            end
+        end
+        writefile(kVars.txtName, kVars.HttpService:JSONEncode(kVars.themes))
+    else
+        print("You do not have injector dose not support writefile.")
+    end
+end
+
+---- Destroy multiple instances ----
+if game:GetService('CoreGui'):FindFirstChild(kVars.WindowName) then
+   game:GetService('CoreGui'):FindFirstChild(kVars.WindowName):Destroy()
+   wait(.2)
+end
+
+---- antiAFK ----
+kVars.AntiAfk = game:GetService('Players').LocalPlayer.Idled:connect(function()
+   kVars.vu:CaptureController()
+   kVars.vu:ClickButton2(Vector2.new())
 end)
 
---- Dig ---
-f:Toggle("Dig",function(bool)
-    kVars.boolDig = bool
-    if bool then fDig() end
+---- Load window ----
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/zxciaz/VenyxUI/main/Reuploaded"))() --someone reuploaded it so I put it in place of the original back up so guy can get free credit.
+local Window = library.new(kVars.WindowName, 5013109572)
+
+---- pages ----
+local pageFarm = Window:addPage("Farm", 3117485989)
+local pagePlayer = Window:addPage("Player", 5012544693)
+local pageSettings = Window:addPage("Settings", 6942070576)
+local sectionAutoFarm = pageFarm:addSection("Auto Farm")
+local sectionPlayerStats = pagePlayer:addSection("Stats")
+local sectionTpToPlayer = pagePlayer:addSection("Teleport To Player")
+local sectionTheme = pageSettings:addSection("Theme")
+
+----  ----
+sectionAutoFarm:addToggle("Dig", false, function(bool)
+   kVars.boolDig = bool
+   if bool then fDig() end
 end)
+
 function fDig()
     spawn(function()
         while kVars.boolDig do
@@ -31,48 +87,115 @@ function fDig()
     end)
 end
 
---- Sell ---
-f:Toggle("Sell",function(bool)
+sectionAutoFarm:addToggle("Sell", false, function(bool)
     kVars.boolSell = bool
     if bool then fSell() end
-end)
-function fSell()
-    spawn(function()
-        while kVars.boolSell do
-            wait()
-            game.Workspace.__REMOTES.Sell:FireServer()
-        end
-    end)
-end
+ end)
+ 
+ function fSell()
+     spawn(function()
+         while kVars.boolSell do
+             wait()
+             game.Workspace.__REMOTES.Sell:FireServer()
+         end
+     end)
+ end
 
---- rebirth ---
-f:Toggle("Rebirth",function(bool)
+ sectionAutoFarm:addToggle("Rebirth", false, function(bool)
     kVars.boolRebirth = bool
     if bool then fRebirth() end
-end)
-function fRebirth()
-    spawn(function()
-        while kVars.boolRebirth do
-            wait()
-            if game:GetService("Players").LocalPlayer:FindFirstChild("leaderstats") and game:GetService("Players").LocalPlayer.leaderstats:FindFirstChild("Level") then
-                local level = game:GetService("Players").keathunsar.leaderstats.Level.Value
+ end)
+ 
+ function fRebirth()
+     spawn(function()
+         while kVars.boolRebirth do
+             wait()
+             if game:GetService("Players").LocalPlayer:FindFirstChild("leaderstats") and game:GetService("Players").LocalPlayer.leaderstats:FindFirstChild("Level") then
+                local level = game:GetService("Players").LocalPlayer.leaderstats.Level.Value
                 if (level >= 125) then
                     game.Workspace.__REMOTES.Rebirth:InvokeServer()
                 end
             end
-        end
-    end)
-end
+         end
+     end)
+ end
 
+---- player stats section ----
+sectionPlayerStats:addSlider("Walk Speed", 16, kVars.lp.Character:WaitForChild('Humanoid').WalkSpeed, 1024, function(value)
+    kVars.lp.Character:WaitForChild('Humanoid').WalkSpeed = value
+end)
 
----- Closing ----
-kVars.closeing = game:GetService("CoreGui").ChildRemoved:Connect(function(child)
-	if child.Name == kVars.windowName then
-		kVars.closing = true
-		wait(.1)
-		table.clear(kVars)
-		kVars.closeing:Disconnect()
-		script:Destroy()
+sectionPlayerStats:addSlider("Jump Power", 50, kVars.lp.Character:WaitForChild('Humanoid').JumpPower, 1024, function(value)
+    kVars.lp.Character:WaitForChild('Humanoid').JumpPower = value
+end)
 
+sectionPlayerStats:addToggle("Inf Jump", false, function(bool)
+    kVars.boolInfJump = bool
+end)
+
+kVars.InfJumpConnect = game:GetService("UserInputService").JumpRequest:connect(function()
+	if kVars.boolInfJump and kVars.lp.Character:FindFirstChild("Humanoid") then
+		kVars.lp.Character:FindFirstChildOfClass('Humanoid'):ChangeState("Jumping")
 	end
 end)
+
+---- tp to player section ----
+sectionTpToPlayer:addTextbox("PlayerName", "Name", function(value, focusLost)
+    if focusLost then
+        kVars.teleportToPlayer = value
+        print(value)
+    end
+end)
+
+sectionTpToPlayer:addButton("Teleport", function()
+    local cnf = true
+    if game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart") and kVars.teleportToPlayer ~= nil then
+        for i,v in pairs(game:GetService("Players"):GetChildren()) do
+            local hrp = game:GetService('Players').LocalPlayer.Character:WaitForChild('HumanoidRootPart')
+            if string.find(string.lower(v.DisplayName), string.lower(kVars.teleportToPlayer)) then
+                hrp.CFrame = v.Character.HumanoidRootPart.CFrame
+                cnf = false
+            elseif string.find(string.lower(v.Name), string.lower(kVars.teleportToPlayer)) then
+                hrp.CFrame = v.Character.HumanoidRootPart.CFrame
+                cnf = false
+            end
+        end
+        if cnf == true then
+            Window:Notify("Error", "Could not find player.")
+        end
+    else
+        Window:Notify("Error", "You didn't put a valid playername in the textbox.")
+    end
+end)
+
+---- Theme ----
+for theme, color in pairs(kVars.themes) do
+    sectionTheme:addColorPicker(theme, color, function(color3)
+        Window:setTheme(theme, color3)
+    end)
+    Window:setTheme(theme, color)
+end
+
+sectionTheme:addButton("Save Theme", function()
+    saveSettings()
+end)
+---- Select main page to display ----
+Window:SelectPage(Window.pages[1], true)
+
+---- Closing ----
+kVars.ClosingConnect = game:GetService("CoreGui").ChildRemoved:Connect(function(child)
+	if child.Name == kVars.WindowName then
+	    kVars.InfJumpConnect:Disconnect()
+        kVars.AntiAfk:Disconnect()
+		for i,v in pairs(kVars) do
+			if type(v) == "boolean" then
+				v = false
+			end
+		end
+        wait(.1)
+		kVars.ClosingConnect:Disconnect()
+		script:Destroy()
+	end
+end)
+
+syn.protect_gui(game:GetService("CoreGui")[kVars.WindowName])
