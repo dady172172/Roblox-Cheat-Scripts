eb56dd59974450daf8b7baeca32740f0fb6e5744
Kethunsar <dady172172@gmail.com>
2022-07-07 23:44:11 -0400

    Added Texting Simulator GUI
    Signed-off-by: Kethunsar <dady172172@gmail.com>

---
+++ Texting Simulator GUI.lua
@@ -0,0 +1,253 @@
+--[[
+Game : https://www.roblox.com/games/2580982329
+Coded by : Keathunsar : https://github.com/dady172172/Roblox-Cheats
+Gui made by : https://v3rmillion.net/member.php?action=profile&uid=244024
+]]--
+
+---- Variables ----
+kVars = {}
+kVars.WindowName = "Texting Simulator GUI"
+kVars.txtName = kVars.WindowName .. ".txt"
+kVars.lp = game:GetService('Players').LocalPlayer
+kVars.rs = game:GetService('ReplicatedStorage')
+kVars.vu = game:GetService('VirtualUser')
+kVars.HttpService = game:GetService("HttpService")
+kVars.themes = {}
+kVars.tmp = {}
+---- Load Settings ----
+if readfile and isfile and isfile(kVars.txtName) then
+    kVars.tmp = kVars.HttpService:JSONDecode(readfile(kVars.txtName))
+    for i,v in pairs(kVars.tmp) do
+        kVars.themes[i] = Color3.fromRGB(v[1], v[2], v[3])
+    end
+else
+    print("Loading defult theme")
+    kVars.themes = {
+        Background = Color3.fromRGB(24, 24, 24),
+        Glow = Color3.fromRGB(0, 0, 0),
+        Accent = Color3.fromRGB(10, 10, 10),
+        LightContrast = Color3.fromRGB(20, 20, 20),
+        DarkContrast = Color3.fromRGB(14, 14, 14),  
+        TextColor = Color3.fromRGB(255, 255, 255)
+   }
+end
+
+---- Save Settings ----
+function saveSettings()
+    if writefile then
+        kVars.themes = {}
+        for i,v in pairs(game:GetService('CoreGui')[kVars.WindowName]:GetChildren()) do
+            if v.Name == "ColorPicker" then
+                kVars.themes[v.Title.Text] = {v.Container.Inputs.R.Textbox.text, v.Container.Inputs.G.Textbox.text, v.Container.Inputs.B.Textbox.text}
+            end
+        end
+        writefile(kVars.txtName, kVars.HttpService:JSONEncode(kVars.themes))
+    else
+        print("You injector does not support writefile.")
+    end
+end
+
+---- Destroy multiple instances ----
+if game:GetService('CoreGui'):FindFirstChild(kVars.WindowName) then
+   game:GetService('CoreGui'):FindFirstChild(kVars.WindowName):Destroy()
+   wait(.2)
+end
+
+---- antiAFK ----
+kVars.AntiAfk = game:GetService('Players').LocalPlayer.Idled:connect(function()
+   kVars.vu:CaptureController()
+   kVars.vu:ClickButton2(Vector2.new())
+end)
+
+---- Load window ----
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/zxciaz/VenyxUI/main/Reuploaded"))()
+local Window = library.new(kVars.WindowName, 5013109572)
+
+---- pages and sections ----
+local pageFarm = Window:addPage("Farm", 3117485989)
+local sectionAutoFarm = pageFarm:addSection("Auto Farm")
+
+local pagePlayer = Window:addPage("Player", 5012544693)
+local sectionPlayerStats = pagePlayer:addSection("Stats")
+local sectionTpToPlayer = pagePlayer:addSection("Teleport To Player")
+
+local pageSettings = Window:addPage("Settings", 6942070576)
+local sectionTheme = pageSettings:addSection("Theme")
+local sectionKeybinds = pageSettings:addSection("Keybinds")
+
+----  ----
+sectionAutoFarm:addToggle("Send Texts", false, function(bool)
+    kVars.boolSendTexts = bool
+    if bool then fSendTexts() end
+end)
+
+function fSendTexts()
+    spawn(function()
+        while kVars.boolSendTexts do
+            wait()
+            pcall(function()
+                kVars.rs.Events.SendTexts:FireServer("Phone")
+            end)
+        end
+    end)
+end
+
+sectionAutoFarm:addToggle("Sell", false, function(bool)
+    kVars.boolSell = bool
+    if bool then fSell() end
+end)
+
+function fSell()
+    spawn(function()
+        while kVars.boolSell do
+            pcall(function()
+                firetouchinterest(kVars.lp.Character.HumanoidRootPart,game:GetService("Workspace").Scriptables.SellPlatform["Meshes/riong"],0)
+                firetouchinterest(kVars.lp.Character.HumanoidRootPart,game:GetService("Workspace").Scriptables.SellPlatform["Meshes/riong"],1)
+                
+            end)
+            wait(1)
+        end
+    end)
+end
+
+sectionAutoFarm:addToggle("Coins *lags", false, function(bool)
+    kVars.boolCoins = bool
+    if bool then fCoins() end
+end)
+
+function fCoins()
+    spawn(function()
+        while kVars.boolCoins do
+            pcall(function()
+                for i,v in pairs(game:GetService("Workspace").Coins:GetChildren()) do 
+                    v.CFrame = kVars.lp.Character.HumanoidRootPart.CFrame
+                    wait()
+                    if kVars.boolCoins == false then break end
+                end
+            end)
+        end
+    end)
+end
+
+sectionAutoFarm:addToggle("Gems *lags. use hack term below for gems", false, function(bool)
+    kVars.boolGems = bool
+    if bool then fGems() end
+end)
+
+function fGems()
+    spawn(function()
+        while kVars.boolGems do
+            pcall(function()
+                for i,v in pairs(game:GetService("Workspace").Gems:GetChildren()) do
+                    v.CFrame = kVars.lp.Character.HumanoidRootPart.CFrame
+                    wait()
+                    if kVars.boolGems == false then break end 
+                end
+            end)
+        end
+    end)
+end
+
+sectionAutoFarm:addToggle("Hack Terminal", false, function(bool)
+    kVars.boolHackTerm = bool
+    if bool then fHackTerm() end
+end)
+
+function fHackTerm()
+    spawn(function()
+        local a = game:GetService("Workspace").HackingTerminals.Hack3.HackColor.Wedge.Rewards
+        while kVars.boolHackTerm do
+            task.wait()
+            game:GetService("ReplicatedStorage").Events.HackingTermStart:FireServer(3)
+            game:GetService("ReplicatedStorage").Events.TerminalReward:FireServer(a)
+        end
+    end)
+end
+
+
+
+---- player stats section ----
+sectionPlayerStats:addSlider("Walk Speed", 16, kVars.lp.Character:WaitForChild('Humanoid').WalkSpeed, 1024, function(value)
+    kVars.lp.Character:WaitForChild('Humanoid').WalkSpeed = value
+end)
+
+sectionPlayerStats:addSlider("Jump Power", 50, kVars.lp.Character:WaitForChild('Humanoid').JumpPower, 1024, function(value)
+    kVars.lp.Character:WaitForChild('Humanoid').JumpPower = value
+end)
+
+sectionPlayerStats:addToggle("Inf Jump", false, function(bool)
+    kVars.boolInfJump = bool
+end)
+
+kVars.InfJumpConnect = game:GetService("UserInputService").JumpRequest:connect(function()
+	if kVars.boolInfJump and kVars.lp.Character:FindFirstChild("Humanoid") then
+		kVars.lp.Character:FindFirstChildOfClass('Humanoid'):ChangeState("Jumping")
+	end
+end)
+
+---- tp to player section ----
+sectionTpToPlayer:addTextbox("PlayerName", "Name", function(value, focusLost)
+    if focusLost then
+        kVars.teleportToPlayer = value
+        print(value)
+    end
+end)
+
+sectionTpToPlayer:addButton("Teleport", function()
+    local cnf = true
+    if game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart") and kVars.teleportToPlayer ~= nil then
+        for i,v in pairs(game:GetService("Players"):GetChildren()) do
+            local hrp = game:GetService('Players').LocalPlayer.Character:WaitForChild('HumanoidRootPart')
+            if string.find(string.lower(v.DisplayName), string.lower(kVars.teleportToPlayer)) then
+                hrp.CFrame = v.Character.HumanoidRootPart.CFrame
+                cnf = false
+            elseif string.find(string.lower(v.Name), string.lower(kVars.teleportToPlayer)) then
+                hrp.CFrame = v.Character.HumanoidRootPart.CFrame
+                cnf = false
+            end
+        end
+        if cnf == true then
+            Window:Notify("Error", "Could not find player.")
+        end
+    else
+        Window:Notify("Error", "You didn't put a valid playername in the textbox.")
+    end
+end)
+
+---- Theme ----
+for theme, color in pairs(kVars.themes) do
+    sectionTheme:addColorPicker(theme, color, function(color3)
+        Window:setTheme(theme, color3)
+    end)
+    Window:setTheme(theme, color)
+end
+
+sectionTheme:addButton("Save Theme", function()
+    saveSettings()
+end)
+
+sectionKeybinds:addKeybind("Open/Close Menu", Enum.KeyCode.Backquote, function()
+	Window:toggle()
+end, function()
+end)
+
+---- Select main page to display ----
+Window:SelectPage(Window.pages[1], true)
+
+---- Closing ----
+kVars.ClosingConnect = game:GetService("CoreGui").ChildRemoved:Connect(function(child)
+	if child.Name == kVars.WindowName then
+	    kVars.InfJumpConnect:Disconnect()
+        kVars.AntiAfk:Disconnect()
+		for i,v in pairs(kVars) do
+			if type(v) == "boolean" then
+				v = false
+			end
+		end
+        wait(.1)
+		kVars.ClosingConnect:Disconnect()
+		script:Destroy()
+	end
+end)
+
+syn.protect_gui(game:GetService("CoreGui")[kVars.WindowName])
